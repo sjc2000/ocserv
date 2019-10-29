c7be962d2739dbd51ebe477aad46a2a0f63f69df
sjc2000 <sjc9000@gmail.com>
2019-10-29 20:17:53 +0800

    Update mydns.sh

--- mydns.sh
+++ mydns.sh
@@ -1,4 +1,4 @@
 # cat mydns
 wget -O /dev/nul "https://api.dynu.com/nic/update?hostname=any.DOMAIN.com&username=USERNAME@gmail.com&password=MD5ofPASSWORD"
-
-
+# 或
+wget -O regdns.log "https://api.dynu.com/nic/update?myip=172.172.16.26&hostname=any.domainname.com&username=username@gmail.com&password=md5"
